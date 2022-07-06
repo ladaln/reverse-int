676a9e5d0cdbba255fee3bc6688109c7c9a5d9d2
Lada <100614341+ladaln@users.noreply.github.com>
2022-07-06 21:35:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = reverse = (n) => {
+	let result = 0;
+	let t = n;
+	while (t) {
+		result = result * 10 + t % 10;
+		t = Math.floor(t / 10);
+	}
+	return result;
 }
